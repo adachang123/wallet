ee1e353727736cdcee5582d32e5150f4fc29760a
Ada Chang <adachang123@gmail.com>
2018-09-13 14:31:47 +0800

    Add & Delete wallet, add money, calc balance

--- App.jsx
+++ App.jsx
@@ -3,27 +3,71 @@ import React from 'react';
 class App extends React.Component {
     constructor() {
         super();
+        this.idx_count = 0;
         this.state = {
-            wallets: [{
-                id: 1,
-                address: 'xx123',
-                balance: 500
-            }, {
-                id: 2,
-                address: 'xx456',
-                balance: 10
-            }]
+            wallets:[],
+            balance: 0
         };
     }
-   render() {
+    
+    render() {
       return (
         <div>
-            <Header></Header>
-            <ActionBar></ActionBar>
-            <WalletContainer wallets={this.state.wallets}></WalletContainer>
+            <Header balance={this.state.balance}></Header>
+            <ActionBar addWallet={this.addWallet.bind(this)}></ActionBar>
+            <WalletContainer wallets={this.state.wallets}
+                delete={this.deleteWallet.bind(this)}
+                addMoney={this.addMoney.bind(this)}
+            ></WalletContainer>
         </div>
       );
    }
+
+   addWallet() {
+        let new_wallet = {
+            id: this.idx_count++,
+            address: new Date().valueOf(),
+            balance: 0
+        };
+        this.state.wallets.push(new_wallet);
+        this.setState(this.state);
+    }
+
+    getIdxByWalletId(id) {
+        for (var idx in this.state.wallets) {
+            if (this.state.wallets[idx].id == id) {
+                return idx;
+            }
+        }
+        return -1;
+    }
+
+    deleteWallet(id) {
+        let idx = this.getIdxByWalletId(id);
+
+        if (idx != -1) {
+            this.state.wallets.splice(idx,1);
+            this.updateBalance();
+            this.setState(this.state);
+        } 
+    }
+
+    updateBalance() {
+        let balance = 0;
+        for (let i in this.state.wallets) {
+            balance += this.state.wallets[i].balance;
+        }
+        this.state.balance = balance;
+    }
+    addMoney(id) {
+        let idx = this.getIdxByWalletId(id);
+
+        if (idx != -1) {
+            this.state.wallets[idx].balance += 10;
+            this.updateBalance();
+            this.setState(this.state);
+        }
+    }
 }
 
 export default App;
@@ -32,18 +76,22 @@ class Header extends React.Component {
     render() {
         return (
             <div className="page-header">
-                Test Wallet
+                <h2>Miao Wallet</h2>
+                <div className="balance">Balance: {this.props.balance} MIAO</div>
             </div>
         );
     }
 }
 
 class ActionBar extends React.Component {
+    tmpFun() {
+        alert('Miao');
+    }
     render() {
         return (
             <div className="action-bar">
-                <button>Create Wallet</button>
-                <button>Import Wallet</button>
+                <button onClick={this.props.addWallet}>Create Wallet</button>
+                <button onClick={this.tmpFun}>Import Wallet</button>
             </div>
         )
     };
@@ -54,7 +102,10 @@ class WalletContainer extends React.Component {
         return (
             <div className="wallet-container">
                 {this.props.wallets.map(wallet => 
-                    <Wallet key={wallet.id} {...wallet}></Wallet>
+                    <Wallet key={wallet.id} 
+                        delete={this.props.delete}
+                        addMoney={this.props.addMoney} {...wallet}>
+                    </Wallet>
                 )}
             </div>
         )
@@ -65,8 +116,19 @@ class Wallet extends React.Component {
     render() {
         return (
             <div className="wallet">
-                address: {this.props.address} <br/>
-                balance: {this.props.balance}
+                <div className="info">
+                    id: {this.props.id} <br />
+                    address: {this.props.address} <br/>
+                    balance: {this.props.balance} MIAO
+                </div>
+                <div className="btns">
+                    <button className="delete-btn"
+                        onClick={() => this.props.delete(this.props.id)}>Delete
+                    </button>
+                    <button className="add-money-btn"
+                        onClick={() => this.props.addMoney(this.props.id)}>Add some money
+                    </button>
+                </div>           
             </div>
         )
     };
